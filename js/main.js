84b4b927e300b4e4c55706e092e66d0c9082bd7d
i-sak <isaac7263@naver.com>
2020-08-09 21:15:35 +0900

    js 수치 수정

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function chkSize(e){
         for(var i = 0 ; i < boxs.length; i++) {
             boxs[i].children[0].firstElementChild.style.width = "50px";
             boxs[i].children[0].firstElementChild.style.height = "50px";
-            boxs[i].style.fontSize = "11px";
+            boxs[i].style.fontSize = "12px";
             boxs[i].style.width = "100px";
             boxs[i].style.height = "150px";
             // 섹션
