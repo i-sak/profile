dbf3a55c4a68a31e0057768d3d54b0e25f063fbe
i-sak <isaac7263@naver.com>
2020-09-08 03:16:10 +0900

    화면 마스크

--- js/main.js
+++ js/main.js
@@ -81,3 +81,36 @@ function descConfirm() {
         return;
     }
 }
+
+function wrapWindowByMask(){
+    //화면의 높이와 너비를 구한다.
+    var maskHeight = $(document).height();  
+    var maskWidth = $(window).width();  
+    
+    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채운다.
+    $('#mask').css({'width':"100%",'height':maskHeight});  
+    $('#mask_btn').fadeIn(1000);
+
+    //애니메이션 효과
+    //$('#mask').fadeIn(1000);      
+    $('#mask').fadeTo("slow",0.8);    
+
+    //윈도우 같은 거 띄운다.
+    $('.window').css( {'color': 'white' } );
+    $('.window').show();
+}
+jQuery(document).ready(function(){ 
+
+    //닫기 버튼을 눌렀을 때
+    $('.window .close').click(function (e) {  
+        //링크 기본동작은 작동하지 않도록 한다.
+        e.preventDefault();  
+        $('#mask, .window').hide();  
+    });       
+
+    //검은 막을 눌렀을 때
+    $('#mask').click(function () {  
+        $(this).hide();  
+        $('.window').hide();  
+    });
+});
